9fc3ce4691e13d197aa8c57b5d595c2266143bec
rudol175 <rudol175@umn.edu>
2018-11-25 13:28:05 -0600

    Uploading basic chatbot

---
+++ Project/ChatBot.py
@@ -0,0 +1,84 @@
+import nltk
+import numpy as np
+import random
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+f = open('chatbot.txt', 'r', errors = 'ignore')
+
+raw = f.read()
+
+raw = raw.lower()
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
+
+sent_tokens[:2]
+word_tokens[:5]
+
+lemmer = nltk.stem.WordNetLemmatizer()
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+GREETING_INPUTS = ("hello", "hi", "sup", "what's up?", "hey", "heyy", "greetings slut",)
+
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "heya", "hello", "I am so glad I finally have a friend to talk to."]
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+def response(user_response):
+    robo_response=''
+
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+
+    if(req_tfidf==0):
+        robo_response = robo_response + "What the fuck are you saying?"
+        return robo_response
+    else:
+        robo_response = robo_response + sent_tokens[idx]
+        return robo_response
+
+flag = True
+
+print("Bot: My name is dipshit. I will answer questions about chatbots as stupid as me! If you are fed up with me, tell me Bye")
+
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if (user_response=='thanks' or user_response=='thank you'):
+            flag = False
+            print("Bot: You're welcome")
+        else:
+            if(greeting(user_response) != None):
+                print("Bot: " + greeting(user_response))
+            else:
+                sent_tokens.append(user_response)
+
+                word_tokens = word_tokens + nltk.word_tokenize(user_response)
+                final_words = list(set(word_tokens))
+                print("Bot: ", end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("Bot: Cya bitch")
