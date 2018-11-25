7912c9d4a3dd00d3854ff33c4b4b98922bae19dc
rudol175 <rudol175@umn.edu>
2018-11-25 16:44:03 -0600

    Adding grammar parsing.

--- project/chatbot.py
+++ project/chatbot.py
@@ -1,3 +1,9 @@
+# Mitchell Rudoll and Oliver Whittlef
+
+# Inspiration drawn from Eliza and https://github.com/lizadaly/brobot/blob/master/broize.py
+
+# IMPORTS
+
 from __future__ import print_function, unicode literals
 import nltk
 import numpy as np
@@ -8,6 +14,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import os
 from textblob import TextBlob
 from config import FILTER_WORDS, GREETING_INPUTS, GREETING_RESPONSES, NONE_RESPONSES, COMMENTS_ABOUT_SELF
+
+# DATA LOADING
 
 os.environ['NLTK_DATA'] = os.getcwd() + '/nltk_data'
 
@@ -26,6 +34,83 @@ word_tokens = nltk.word_tokenize(raw)
 sent_tokens[:2]
 word_tokens[:5]
 
+# API CALLS
+
+# CLASSES
+
+class NoNoWordsException(Exception):
+    """Response triggered blacklist"""
+    pass
+
+# FUNCTIONS
+
+def starts_with_vowel(word):
+    """Check for pronoun compability -- 'a' vs. 'an'"""
+    return True if word[0] in 'aeiou' else False
+
+def find_pronoun(sent):
+    """Given a sentence, find a preferred pronoun to respond with. Returns None if no candidate
+    pronoun is found in the input"""
+    pronoun = None
+
+    for word, part_of_speech in sent.pos_tags:
+        # Disambiguate pronouns
+        if part_of_speech == 'PRP' and word.lower() == 'you':
+            pronoun = 'I'
+        elif part_of_speech == 'PRP' and word == 'I':
+            # If the user mentioned themselves, then they will definitely be the pronoun
+            pronoun = 'You'
+    return pronoun
+
+def find_verb(sent):
+    """Pick a candidate verb for the sentence."""
+    verb = None
+    pos = None
+    for word, part_of_speech in sent.pos_tags:
+        if part_of_speech.startswith('VB'):  # This is a verb
+            verb = word
+            pos = part_of_speech
+            break
+    return verb, pos
+
+
+def find_noun(sent):
+    """Given a sentence, find the best candidate noun."""
+    noun = None
+
+    if not noun:
+        for w, p in sent.pos_tags:
+            if p == 'NN':  # This is a noun
+                noun = w
+                break
+    if noun:
+        logger.info("Found noun: %s", noun)
+
+    return noun
+
+def find_adjective(sent):
+    """Given a sentence, find the best candidate adjective."""
+    adj = None
+    for w, p in sent.pos_tags:
+        if p == 'JJ':  # This is an adjective
+            adj = w
+            break
+    return adj
+
+def preprocess_text(sentence):
+    """Handle some weird edge cases in parsing, like 'i' needing to be capitalized
+    to be correctly identified as a pronoun"""
+    cleaned = []
+    words = sentence.split(' ')
+    for w in words:
+        if w == 'i':
+            w = 'I'
+        if w == "i'm":
+            w = "I'm"
+        cleaned.append(w)
+
+    return ' '.join(cleaned)
+
 lemmer = nltk.stem.WordNetLemmatizer()
 
 def LemTokens(tokens):
@@ -37,7 +122,7 @@ def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 def greeting(sentence):
-    for word in sentence.split():
+    for word in sentence.word:
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
 
@@ -58,6 +143,8 @@ def response(user_response):
     else:
         robo_response = robo_response + sent_tokens[idx]
         return robo_response
+
+# DRIVER
 
 flag = True
 
