4e746563eee190f62dad3777714b469585305a70
Oliver Whittlef <whit243@umn.edu>
2018-12-01 23:12:28 -0600

    Api returns better type

--- api/interaction.py
+++ api/interaction.py
@@ -11,11 +11,18 @@ URL = 'https://rxnav.nlm.nih.gov/REST/interaction/list.json'
 #   names = ('tylenol', 'ibuprofen', 'viagra')
 #   findDrugInteractions(names)
 
-def findDrugInteractions(names):
-    rxcuis = map(rxNormId, names)
+def findDrugInteractions(rxcuis):
     resp = requests.get(URL + '?rxcuis=' + '+'.join(rxcuis))
+    interactions = {}
     if resp.status_code != 200:
         raise ApiError('GET {0} returned {1}'.format(resp.url, resp.status_code))
     for interaction_type in resp.json()['fullInteractionTypeGroup'][0]['fullInteractionType']:
         for interaction_pair in interaction_type['interactionPair']:
-            print(interaction_pair['description'])
+            d1 = interaction_type['minConcept'][0]['rxcui']
+            d2 = interaction_type['minConcept'][1]['rxcui']
+            interactions['{0},{1}'.format(d1, d2)] = interaction_pair['description']
+    return interactions
+
+# TEST:
+# names = ('tylenol', 'ibuprofen', 'viagra')
+# print(findDrugInteractions(map(rxNormId, names)))
