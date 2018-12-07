9ecbfb6205929458b34e9be601c1c4470b79335e
oliver whittlef <joliverw@gmail.com>
2018-12-07 01:32:16 -0600

    Got rid of unused urls

--- website/bot/urls.py
+++ website/bot/urls.py
@@ -6,10 +6,4 @@ urlpatterns = [
     # ex: /bot/
     path('', views.index, name='index'),
     path('converse', views.conversation_view)
-    # ex: /bot/5/
-    # path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /bot/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /bot/5/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- website/bot/views.py
+++ website/bot/views.py
@@ -22,16 +22,3 @@ def index(request):
     bot_response = converse('hello')
     context = {'bot_response': bot_response}
     return render(request, 'bot/index.html', context)
-
-@csrf_exempt
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-@csrf_exempt
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-@csrf_exempt
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
