3aef2def764253953673d29d3641f70032ac696d
oliver whittlef <joliverw@gmail.com>
2018-12-08 11:33:02 -0600

    More prettiness

--- website/bot/static/bot/medbot.js
+++ website/bot/static/bot/medbot.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
     sessionStorage.clear();
 });
+
+// Scroll to the bottom of ele, and play scroll animation
+function scroll(ele) {
+    // var height = height < ele.scrollHeight ? ele.scrollHeight : 0;
+    var height = ele.scrollHeight;
+    this.stop().animate({
+      scrollTop: height
+    }, 1000});
+  };
