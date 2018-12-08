7a3532e38c37135a3d78322594559902dcd2967f
oliver whittlef <joliverw@gmail.com>
2018-12-08 15:40:01 -0600

    scroll to bottom on user input

--- website/bot/static/bot/medbot.js
+++ website/bot/static/bot/medbot.js
@@ -1,13 +1,4 @@
 $( document ).ready(function() {
     sessionStorage.clear();
-    $("#copyrightYear").text(new Date().getFullYear()); 
+    $("#copyrightYear").text(new Date().getFullYear());
 });
-
-// Scroll to the bottom of ele, and play scroll animation
-function scroll(ele) {
-    // var height = height < ele.scrollHeight ? ele.scrollHeight : 0;
-    var height = ele.scrollHeight;
-    this.stop().animate({
-      scrollTop: height
-    }, 1000);
-  };
