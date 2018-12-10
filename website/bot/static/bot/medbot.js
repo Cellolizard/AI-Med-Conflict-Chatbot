e993db534726c7547a1f27b03170d2bbbbbf9192
oliver whittlef <joliverw@gmail.com>
2018-12-09 21:52:35 -0600

    I just want the latest geez

--- website/bot/static/bot/medbot.js
+++ website/bot/static/bot/medbot.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
     sessionStorage.clear();
     $("#copyrightYear").text(new Date().getFullYear());
+    $('[data-toggle="popover"]').popover();
 });
+$('[data-toggle="popover"]').popover({
+  gpuAcceleration: !(window.devicePixelRatio < 1.5 && /Win/.test(navigator.platform))
+})
